f22b9ca8b04f31fed0245cc0e3cebc2feabefa72
whyis2plus2 <146612147+whyis2plus2@users.noreply.github.com>
2024-11-30 00:31:15 -0700

    add hex code for tag colors

--- src/YAJF/UI/Dialogs/TagSettings.cs
+++ src/YAJF/UI/Dialogs/TagSettings.cs
@@ -15,30 +15,29 @@ using System.Timers;
 
 public class TagSettings: CanvasSingleton<TagSettings>
 {
-    InputField field;
+    InputField field, Hex;
     Slider Red, Green, Blue;
     byte r = 255, g = 255, b = 255;
 
     private void Start()
     {
-        if (Prefs.tagColor != null)
+        if (Prefs.tagColor.Length >= 7)
         {
             r = Convert.ToByte("0x" + Prefs.tagColor.Substring(1, 2), 16);
             g = Convert.ToByte("0x" + Prefs.tagColor.Substring(3, 2), 16);
             b = Convert.ToByte("0x" + Prefs.tagColor.Substring(5, 2), 16);
         }
 
-        UIB.Table("Content", "---TAG SETTINGS---", transform, Size(640, 356), table =>
+        UIB.Table("Content", "---TAG SETTINGS---", transform, Size(640, 395), table =>
         {
             UIB.IconButton("X", table, Icon(292f, 24f), red, clicked: Toggle);
 
             field = UIB.Field("TAG", table, new(320f, -68f, 350f, 40f, new(0f, 1f)), cons: text =>
             {
-                Prefs.tagColor = $"#{r:X}{g:X}{b:X}";
+                Prefs.tagColor = $"#{r:X2}{g:X2}{b:X2}";
                 Prefs.tag = Regex.Replace(text.Trim(), "<*.?>", "").Replace("[", "\\[");
             });
-
-            field.characterLimit = Prefs.MAX_TAG_LEN - 9 /* length of color and terminator combined */;
+            field.characterLimit = Prefs.MAX_TAG_LEN;
 
             UIB.Text("Red", table, Btn(108), red, align: TextAnchor.MiddleLeft);
             var RText = UIB.Text("0", table, Btn(108), red, align: TextAnchor.MiddleRight);
@@ -67,9 +66,39 @@ public class TagSettings: CanvasSingleton<TagSettings>
                 Rebuild();
             });
 
-            UIB.IconButton("CONFIRM", table, Btn(300), green, clicked: () =>
+            Hex = UIB.Field("Hex", table, new(320f, -283f, 350f, 40f, new(0f, 1f)), cons: text =>
             {
-                Prefs.tagColor = $"#{r:X}{g:X}{b:X}";
+                while (text.Length < 6 && text.Length % 2 == 0) text += "00";
+                if (text.Length == 3)
+                {
+                    r = (byte)(Convert.ToByte("0x" + text.Substring(0, 1), 16) * 0x11);
+                    g = (byte)(Convert.ToByte("0x" + text.Substring(1, 1), 16) * 0x11);
+                    b = (byte)(Convert.ToByte("0x" + text.Substring(2, 1), 16) * 0x11);
+
+                    Prefs.tagColor = $"#{r:X2}{g:X2}{b:X2}";
+                    Rebuild();
+                    return;
+                }
+                if (text.Length != 6)
+                {
+                    r = g = b = 0xFF;
+                    Prefs.tagColor = $"#FFFFFF";
+                    Rebuild();
+                    return;
+                }
+
+                r = Convert.ToByte("0x" + text.Substring(0, 2), 16);
+                g = Convert.ToByte("0x" + text.Substring(2, 2), 16);
+                b = Convert.ToByte("0x" + text.Substring(4, 2), 16);
+
+                Prefs.tagColor = $"#{r:X2}{g:X2}{b:X2}";
+                Rebuild();
+            });
+            Hex.characterLimit = 6;
+
+            UIB.IconButton("CONFIRM", table, Btn(335), green, clicked: () =>
+            {
+                Prefs.tagColor = $"#{r:X2}{g:X2}{b:X2}";
                 Prefs.tag = Regex.Replace(field.text.Trim(), "<*.?>", "").Replace("[", "\\[");
 
                 if (string.IsNullOrEmpty(Prefs.tag)) HudMessageReceiver.Instance?.SendHudMessage($"Succesfully Removed Tag");
@@ -86,6 +115,7 @@ public class TagSettings: CanvasSingleton<TagSettings>
     {
         field.textComponent.color = new Color32(r, g, b, 0xFF);
         field.text = Prefs.tag;
+        Hex.text = $"{r:X2}{g:X2}{b:X2}";
 
         if (r != 0) Red.value = r;
         if (g != 0) Green.value = g;
